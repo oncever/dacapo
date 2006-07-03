7168fd213ad203b14bd55444fc6862fe6dc23443
steveb <steveb@0f4fd6db-9b1c-0410-be70-be3c737d7ab8>
2006-07-03 07:31:47 +0000

    Eclipse now appears to be reliably working.  Need  to set up the conf file etc properly...

--- trunk/dacapobench/benchmarks/bmsrc/dacapo/eclipse/EclipseHarness.java
+++ trunk/dacapobench/benchmarks/bmsrc/dacapo/eclipse/EclipseHarness.java
@@ -7,22 +7,29 @@ import dacapo.parser.Config;
 
 public class EclipseHarness extends Benchmark {
 
+  private static final String wsDirectory = "workspace";
+  
   public EclipseHarness(Config config, File scratch) throws Exception {
     super(config, scratch);
   }
   
   public void preIteration(String size) throws Exception {
     super.preIteration(size);
-    if (preserve)
-      deleteTree(new File(scratch,"eclipse/test-workspace"));
-    File wsdir = new File(scratch,"eclipse/test-workspace");
+
+/*    if (!preserve)
+      deleteTree(new File(scratch, wsDirectory));
+*/    File wsdir = new File(scratch, wsDirectory);
     wsdir.mkdir();
-    unpackZipFileResource(fileInScratch("eclipse/workspace.zip"),wsdir);
+     unpackZipFileResource(fileInScratch("eclipse/plugins/org.eclipse.jdt.core.tests.performance_3.1.2/full-source-R3_0.zip"),wsdir); 
   }
 
   public void iterate(String size) throws Exception {
-    String[] args = config.getArgs(size);
-    
+    System.out.println("Running eclipse benchmark");
+    invokePlugin(config.getArgs(size));
+    System.out.println("Eclipse benchmark complete");
+  }
+
+  private void invokePlugin(String[] args) throws Exception {
     String oldOsgiOs = System.getProperty("osgi.os");
     String oldOsgiWs = System.getProperty("osgi.ws");
     String oldOsgiArch = System.getProperty("osgi.arch");
@@ -31,26 +38,21 @@ public class EclipseHarness extends Benchmark {
       System.setProperty("osgi.ws","gtk");
       System.setProperty("osgi.arch","x86");
       
-      System.out.println("Running eclipse benchmark");
       String[] cmdArgs = new String[] {
-              "-data",fileInScratch("eclipse/test-workspace"),
+              "-data",fileInScratch(wsDirectory),
               "-application","dacapo.eclipse.dacapoHarness",args[0]};
-      for (int i=0; i < cmdArgs.length; i++)
-        System.out.print(cmdArgs[i]+" ");
-      System.out.println();
       new org.eclipse.core.launcher.Main().run(cmdArgs);
-      System.out.println("Eclipse benchmark complete");
     } finally {
       if (oldOsgiOs != null) System.setProperty("osgi.os",oldOsgiOs);
       if (oldOsgiWs != null) System.setProperty("osgi.ws",oldOsgiWs);
       if (oldOsgiArch != null) System.setProperty("osgi.arch",oldOsgiArch);
-    }
+    }  
   }
-
+  
   public void postIteration(String size) throws Exception {
     super.postIteration(size);
     if (!preserve)
-      deleteTree(new File(scratch,"eclipse/test-workspace"));
+      deleteTree(new File(scratch,wsDirectory));
   }
 
 }
