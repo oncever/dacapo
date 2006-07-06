b2243da05c1699ad0b79c9c360bb96fa08d6c16b
steveb <steveb@0f4fd6db-9b1c-0410-be70-be3c737d7ab8>
2006-07-06 01:44:03 +0000

    Tidy up of PASS/FAIL stuff

--- trunk/dacapobench/benchmarks/src/GcCallback.java
+++ trunk/dacapobench/benchmarks/src/GcCallback.java
@@ -5,21 +5,13 @@
 import dacapo.Callback;
 
 public class GcCallback extends Callback {
-  public void startWarmup(String benchmark) {
-    System.err.println("Started warmup " + benchmark);
-    super.startWarmup(benchmark);
-  };
-  public void stopWarmup(String benchmark) {
-    super.stopWarmup(benchmark);
-    System.err.println("Finished warmup " + benchmark);
-    System.err.flush();
-  };
+  /* perform a System.gc() before starting each run */
   public void start(String benchmark) {
     System.gc();
     super.start(benchmark);
   };
-  public void stop(String benchmark) {
-    super.stop(benchmark);
-    System.err.flush();
+  public void startWarmup(String benchmark) {
+    System.gc();
+    super.startWarmup(benchmark);
   };
 }
