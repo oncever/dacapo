e35d415ad0d10648a221e85cf5b8268f354d2618
steveb <steveb@0f4fd6db-9b1c-0410-be70-be3c737d7ab8>
2006-07-06 09:55:29 +0000

    More minor cleanups before doing more work on eclipse.  It seems we need to be able to unzip the workspace for every iteration---too messy otherwise.

--- trunk/dacapobench/benchmarks/bmsrc/dacapo/eclipse/EclipseHarnessThread.java
+++ trunk/dacapobench/benchmarks/bmsrc/dacapo/eclipse/EclipseHarnessThread.java
@@ -6,46 +6,24 @@ import java.net.URL;
 import java.net.URLClassLoader;
 
 public class EclipseHarnessThread extends Thread {
-  /*  private Integer lock;
-   private String[] args;
-   private boolean complete; */
   private EclipseHarness parent;
-//  Class lock;
   URLClassLoader ulc;
-//  Method startMethod;
-//  Method finishMethod;
-//  Method waitMethod;
-//  Method cleanupMethod;
   
   EclipseHarnessThread(EclipseHarness parent, String name) {
     super(name);
     this.parent = parent;
     try {
       URL startupjar = new File(parent.fileInScratch("eclipse/startup.jar")).toURL();
-//      URL build = new File("/home/steveb/devel/dacapo/dacapo-benchmarks/benchmarks/build").toURL();
       URL[] url = {startupjar};
       ulc = new URLClassLoader(url,Thread.currentThread().getContextClassLoader());
-//      lock = Class.forName("dacapo.eclipse.EclipseHarnessLock", true, ulc);
-//      Class[] sig = {Class.forName("[Ljava.lang.String;")};
-//      startMethod = lock.getMethod("start", sig);
-//      finishMethod = lock.getMethod("finish", (Class[]) null);
-//      waitMethod = lock.getMethod("waitToRun", (Class[]) null);
-//      cleanupMethod = lock.getMethod("cleanup", (Class[]) null);
     } catch (Exception e) { e.printStackTrace(); }
-    /*    this.args = null;
-     this.complete = false;
-     this.lock = new Integer(0); */
   }
   
   public void run() {
     try {
-      System.err.println("HT waiting");
       String[] args = EclipseHarnessLock.waitToRun();
-      System.err.println("HT finished waiting");
       invokePlugin(args);
-      System.err.println("HT finished plugin");
     } catch (Exception e) { e.printStackTrace();}
-    System.out.println("plugin finished...");
   }
   
   private void invokePlugin(String[] pluginArgs) throws Exception {
@@ -69,7 +47,6 @@ public class EclipseHarnessThread extends Thread {
       System.setProperty("osgi.ws","gtk");
       System.setProperty("osgi.arch","x86");
       Class launcher = Class.forName("org.eclipse.core.launcher.Main", true, ulc);
-//    System.out.println(launcher.getName()); 
       Class[] sig = {Class.forName("[Ljava.lang.String;")};
       Method m = launcher.getMethod("run", sig);
       Object[] args = {eclipseArgs};
@@ -82,78 +59,4 @@ public class EclipseHarnessThread extends Thread {
       if (oldOsgiArch != null) System.setProperty("osgi.arch",oldOsgiArch);
     }
   }
-  /*
-  public void startBenchmark(String[] args) {
-    try {
-      Object[] a = {args};
-     startMethod.invoke(null, a);
-    } catch (Exception e) { e.printStackTrace(); }
-  }
-  public void finishBenchmark() {
-    try {
-      finishMethod.invoke(null,(Object[]) null);
-    } catch (Exception e) { e.printStackTrace(); }
-  }
-  public void cleanupBenchmark() {
-    try {
-      cleanupMethod.invoke(null,(Object[]) null);
-    } catch (Exception e) { e.printStackTrace(); }
-  } 
-  public String[] waitToRun() {
-    String[] rtn = null;
-    try {
-       rtn = (String[]) waitMethod.invoke(null,(Object[]) null);
-    } catch (Exception e) { e.printStackTrace(); }
-    return rtn;
-  }*/ 
-  /*
-   public void startBenchmark(String[] theArgs) {
-   System.err.println(this.hashCode()+"startBenchmark...");
-   synchronized (lock) {
-   args = theArgs;
-   lock.notify();
-   while (args != null) {
-   try {
-   System.err.println(this.hashCode()+" Starter is waiting...");
-   lock.wait(); 
-   System.err.println(this.hashCode()+" Starter has woken!");
-   } catch (InterruptedException e) { }
-   }
-   System.err.println(this.hashCode()+"starter has finished!");
-   }
-   }
-   public void finishBenchmark() {
-   System.err.println(this.hashCode()+"nearly got a finish!");
-   System.err.flush();
-   synchronized (lock) {
-   System.err.println(this.hashCode()+"got a finish!");
-   args = null;
-   lock.notify();
-   }
-   }
-   public void cleanupBenchmark() {
-   synchronized (lock) {
-   complete = true;
-   lock.notify();
-   }
-   }
-   public String[] waitToRun() {
-   String[] rtn;
-   synchronized (lock) {
-   while (args == null || complete) {
-   try {
-   System.err.println(this.hashCode()+" Waiting to run...");
-   lock.wait(); 
-   System.err.println(this.hashCode()+"done waiting to run...");
-   } catch (InterruptedException e) { }
-   }
-   rtn = complete ? null : args;
-   if (complete) 
-   System.err.println(this.hashCode()+"Got complete!");
-   else
-   System.err.println(this.hashCode()+"Got incomplete!");
-   } 
-   return rtn;
-   }
-   */
 }
