ceca3d41581a7ca46d7c3e770d56fc1be998fa8f
steveb <steveb@users.sourceforge.net>
2006-07-06 09:55:29 +0000

    More minor cleanups before doing more work on eclipse.  It seems we need to be able to unzip the workspace for every iteration---too messy otherwise.

--- trunk/dacapobench/benchmarks/bmsrc/dacapo/eclipse/EclipseHarnessLock.java
+++ trunk/dacapobench/benchmarks/bmsrc/dacapo/eclipse/EclipseHarnessLock.java
@@ -11,19 +11,13 @@ public class EclipseHarnessLock {
       singleton.notify();
       while (singleton.args != null) {
         try {
-          System.err.println(singleton.hashCode()+ "Starter is waiting...");
           singleton.wait(); 
-          System.err.println(singleton.hashCode()+ "Starter has woken!");
         } catch (InterruptedException e) { }
       }
-       System.err.println(singleton.hashCode()+ "starter has finished!");
     }
   }
   public static void finish() {
-    System.err.println(singleton.hashCode()+ "nearly got a finish!");
-    System.err.flush();
     synchronized (singleton) {
-      System.err.println(singleton.hashCode()+ "got a finish!");
       singleton.args = null;
       singleton.notify();
     }
@@ -36,20 +30,13 @@ public class EclipseHarnessLock {
   }
   public static String[] waitToRun() {
     String[] rtn;
-    System.err.println(singleton.hashCode()+ "... about to wait...");
     synchronized (singleton) {
       while (singleton.args == null || singleton.complete) {
         try {
-          System.err.println(singleton.hashCode()+ "XXXXXXXXXX waiting to run...");
           singleton.wait(); 
-          System.err.println(singleton.hashCode()+ "done waiting to run...");
         } catch (InterruptedException e) { }
       }
       rtn = singleton.complete ? null : singleton.args;
-      if (singleton.complete) 
-         System.err.println(singleton.hashCode()+ "Got complete!");
-      else
-        System.err.println(singleton.hashCode()+ "Got incomplete!");
     } 
     return rtn;
   }
