0d9ad8164121bb1a5d522f2bad2b342d1fb99eb3
steveb <steveb@0f4fd6db-9b1c-0410-be70-be3c737d7ab8>
2006-07-06 08:51:33 +0000

    Eclipse can now run multiple iterations, although I need to clean projects before building them, because that is now an issue.
    THere's a fair amount of debugging cruft around too.

--- trunk/dacapobench/extra/eclipse-plugin/src/dacapo/eclipse/HarnessRunner.java
+++ trunk/dacapobench/extra/eclipse-plugin/src/dacapo/eclipse/HarnessRunner.java
@@ -1,6 +1,9 @@
 package dacapo.eclipse;
 
 
+import java.lang.reflect.Method;
+import java.net.URLClassLoader;
+
 import org.eclipse.core.runtime.IPlatformRunnable;
 
 public class HarnessRunner implements IPlatformRunnable {
@@ -11,8 +14,61 @@ public class HarnessRunner implements IPlatformRunnable {
    * @see org.eclipse.core.runtime.IPlatformRunnable
    */
   public Object run(Object args) throws Exception {
-      EclipseTests.initialize();
-      EclipseTests.runtests((String[]) args);
-     return null; 
+    System.err.print(ClassLoader.getSystemClassLoader().hashCode());
+    System.err.println(" " + ClassLoader.getSystemClassLoader());
+    System.err.print(Thread.currentThread().getContextClassLoader().hashCode());
+    System.err.println(" " + Thread.currentThread().getContextClassLoader());
+    System.err.print(Thread.currentThread().getContextClassLoader().getParent().hashCode());
+    System.err.println(" " + Thread.currentThread().getContextClassLoader().getParent());
+    System.err.print(Thread.currentThread().getContextClassLoader().getParent().getParent().hashCode());
+    System.err.println(" " + Thread.currentThread().getContextClassLoader().getParent().getParent());
+    
+    try {
+      do {
+        System.err.println("Starting an iteration!");
+        EclipseTests.initialize();
+        EclipseTests.runtests((String[]) args);
+        System.err.println("Finishing an iteration!");
+        finish();
+        System.err.println("XX Finished!");
+      } while (waitToRun() != null);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    System.err.println("finished all iterations!");
+    return null; 
+  }
+  
+  
+  static Class lock;
+  static Method startMethod;
+  static Method finishMethod;
+  static Method waitMethod;
+  static Method cleanupMethod;
+  
+  static {
+    try {
+    lock = Class.forName("dacapo.eclipse.EclipseHarnessLock", true, ClassLoader.getSystemClassLoader());
+    Class[] sig = {Class.forName("[Ljava.lang.String;")};
+    startMethod = lock.getMethod("start", sig);
+    finishMethod = lock.getMethod("finish", (Class[]) null);
+    waitMethod = lock.getMethod("waitToRun", (Class[]) null);
+    cleanupMethod = lock.getMethod("cleanup", (Class[]) null);
+    } catch (Exception ex){ ex.printStackTrace(); }
+  }
+  
+  public static void finish() {
+    try {
+      finishMethod.invoke(null,(Object[]) null);
+    } catch (Exception e) { e.printStackTrace(); }
+  }
+  
+  public static String[] waitToRun() {
+    String[] rtn = null;
+    try {
+      rtn = (String[]) waitMethod.invoke(null,(Object[]) null);
+    } catch (Exception e) { e.printStackTrace(); }
+    return rtn;
   }
 }
+
