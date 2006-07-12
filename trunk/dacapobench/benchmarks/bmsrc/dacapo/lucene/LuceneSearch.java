f9c83c63ca259b1bdaf1391a1beb6e8987d7edbe
steveb <steveb@users.sourceforge.net>
2006-07-12 05:49:18 +0000

    Numerous minor improvements and fine tuning.

--- trunk/dacapobench/benchmarks/bmsrc/dacapo/lucene/LuceneSearch.java
+++ trunk/dacapobench/benchmarks/bmsrc/dacapo/lucene/LuceneSearch.java
@@ -41,7 +41,8 @@ import dacapo.parser.Config;
 
 /** Simple command-line based search demo. */
 public class LuceneSearch extends dacapo.Benchmark {
-
+  public int completed = 0;
+  
   /** Use the norms from one field for all fields.  Norms are read into memory,
    * using a byte of memory per document per searched field.  This can cause
    * search of large collections with a large number of fields to run out of
@@ -49,25 +50,25 @@ public class LuceneSearch extends dacapo.Benchmark {
    * are all identical, then single norm vector may be shared. */
   private static class OneNormsReader extends FilterIndexReader {
     private String field;
-
+    
     public OneNormsReader(IndexReader in, String field) {
       super(in);
       this.field = field;
     }
-
+    
     public byte[] norms(String field) throws IOException {
       return in.norms(this.field);
     }
   }
-
   
-
+  
+  
   public LuceneSearch(Config config, File scratch) throws Exception {
     super(config, scratch);
   }
-
-
-
+  
+  
+  
   /** Simple command-line based search demo. */
   public void iterate(String size) throws Exception {
     String[] args = config.getArgs(size);
@@ -77,15 +78,15 @@ public class LuceneSearch extends dacapo.Benchmark {
       System.out.println(usage);
       System.exit(0);
     }
-
+    
     String index = "index";
     String field = "contents";
-    String queries = null;
+    String queryBase = null;
+    String outBase = null;
     int repeat = 0;
     boolean raw = false;
     String normsField = null;
     int threads = 1;
-    String outFileName = "lucene-search.out";
     
     for (int i = 0; i < args.length; i++) {
       if ("-index".equals(args[i])) {
@@ -95,7 +96,7 @@ public class LuceneSearch extends dacapo.Benchmark {
         field = args[i+1];
         i++;
       } else if ("-queries".equals(args[i])) {
-        queries = fileInScratch(args[i+1]);
+        queryBase = fileInScratch(args[i+1]);
         i++;
       } else if ("-repeat".equals(args[i])) {
         repeat = Integer.parseInt(args[i+1]);
@@ -108,80 +109,123 @@ public class LuceneSearch extends dacapo.Benchmark {
       } else if ("-threads".equals(args[i])) {
         threads = Integer.parseInt(args[++i]);
       } else if ("-output".equals(args[i])) {
-        outFileName = args[++i];
+        outBase = args[++i];
+      }
+    }
+    completed = 0;
+    for (int j = 0; j < threads; j++) {
+      new QueryThread("Query"+j, j, index, outBase, queryBase, field, normsField, repeat, raw, this).start();
+    }
+    synchronized (this) {
+      while (completed != threads) {
+        try {
+          this.wait();
+        } catch (InterruptedException e) { }
+      }
+    }
+  }
+  
+  
+  public class QueryThread extends Thread {
+    Searcher searcher;
+    BufferedReader in;
+    PrintWriter out;
+    IndexReader reader;
+    String field;
+    int repeat;
+    boolean raw;
+    LuceneSearch parent;
+    
+    public QueryThread(String str, int id, String index, String outBase, String queryBase,
+        String field, String normsField, int repeat, boolean raw, LuceneSearch parent) {
+      super(str);
+      try {
+        reader = IndexReader.open(index);
+        this.field = field;
+        if (normsField != null) reader = new OneNormsReader(reader, normsField);
+        searcher = new IndexSearcher(reader);
+        in = new BufferedReader(new FileReader(queryBase + id + ".txt"));
+        out = new PrintWriter(new BufferedWriter(new FileWriter(fileInScratch(outBase + id))));
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      this.repeat = repeat;
+      this.raw = raw;
+      this.parent = parent;
+    }
+    
+    public void run() {
+      try {
+        runQuery();
+      } catch (Exception e) {
+        e.printStackTrace();
       }
     }
     
-    IndexReader reader = IndexReader.open(index);
-
-    if (normsField != null)
-      reader = new OneNormsReader(reader, normsField);
-
-    Searcher searcher = new IndexSearcher(reader);
-    Analyzer analyzer = new StandardAnalyzer();
-
-    BufferedReader in = null;
-    if (queries != null) {
-      in = new BufferedReader(new FileReader(queries));
-    } else {
-      in = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
-    }
-    
-    PrintWriter out = new PrintWriter(
-            new BufferedWriter(
-                    new FileWriter(fileInScratch(outFileName))));
-
-    while (true) {
-      if (queries == null)                        // prompt the user
-        System.out.print("Query: ");
-
-      String line = in.readLine();
-
-      if (line == null || line.length() == -1)
-        break;
-
-      Query query = QueryParser.parse(line, field, analyzer);
-      out.println("Searching for: " + query.toString(field));
-
-      Hits hits = searcher.search(query);
+    private void runQuery() throws java.io.IOException {
+      Analyzer analyzer = new StandardAnalyzer();
       
-      if (repeat > 0) {                           // repeat & time as benchmark
-        for (int i = 0; i < repeat; i++) {
-          hits = searcher.search(query);
+      while (true) {
+        String line = in.readLine();
+        
+        if (line == null || line.length() == -1)
+          break;
+        
+        Query query = null;
+        try {
+          query = QueryParser.parse(line, field, analyzer);
+        } catch (Exception e){
+          e.printStackTrace(); 
         }
-      }
-
-      out.println(hits.length() + " total matching documents");
-
-      final int HITS_PER_PAGE = 10;
-      for (int start = 0; start < hits.length(); start += HITS_PER_PAGE) {
-        int end = Math.min(hits.length(), start + HITS_PER_PAGE);
-        for (int i = start; i < end; i++) {
-
-          if (raw) {                              // output raw format
-            out.println("doc="+hits.id(i)+" score="+hits.score(i));
-            continue;
+        out.println("Searching for: " + query.toString(field));
+        
+        Hits hits = searcher.search(query);
+        
+        if (repeat > 0) {                           // repeat & time as benchmark
+          for (int i = 0; i < repeat; i++) {
+            hits = searcher.search(query);
           }
-
-          Document doc = hits.doc(i);
-          String path = doc.get("path");
-          if (path != null) {
-            out.println((i+1) + ". " + path);
-            String title = doc.get("title");
-            if (title != null) {
-              out.println("   Title: " + doc.get("title"));
+        }
+        
+        out.println(hits.length() + " total matching documents");
+        
+        final int HITS_PER_PAGE = 10;
+        for (int start = 0; start < hits.length(); start += HITS_PER_PAGE) {
+          int end = Math.min(hits.length(), start + HITS_PER_PAGE);
+          for (int i = start; i < end; i++) {
+            
+            if (raw) {                              // output raw format
+              out.println("doc="+hits.id(i)+" score="+hits.score(i));
+              continue;
             }
-          } else {
-            out.println((i+1) + ". " + "No path for this document");
+            
+            Document doc = hits.doc(i);
+            String path = doc.get("path");
+            if (path != null) {
+              out.println((i+1) + ". " + path);
+              String title = doc.get("title");
+              if (title != null) {
+                out.println("   Title: " + doc.get("title"));
+              }
+            } else {
+              out.println((i+1) + ". " + "No path for this document");
+            }
           }
-        }
-
-        if (queries != null)                      // non-interactive
+          
           break;
         }
+      }
+      out.flush();
+      out.close();
+      reader.close();
+      synchronized (parent) {
+        parent.completed++;
+        if (parent.completed % 4 == 0) {
+          System.out.println(parent.completed+" query batches completed");
+        }
+        parent.notify();
+      }
     }
-    out.flush();
-    out.close();
-    reader.close();
   }
 }
+
