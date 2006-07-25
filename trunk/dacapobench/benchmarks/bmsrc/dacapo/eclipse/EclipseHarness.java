28fa4f728426afd0a13a887e4ad2553ec9faafc7
steveb <steveb@users.sourceforge.net>
2006-07-25 07:41:29 +0000

    Clean up for much simpler eclipse benchmark

--- trunk/dacapobench/benchmarks/bmsrc/dacapo/eclipse/EclipseHarness.java
+++ trunk/dacapobench/benchmarks/bmsrc/dacapo/eclipse/EclipseHarness.java
@@ -11,7 +11,7 @@ import dacapo.parser.Config;
 public class EclipseHarness extends Benchmark {
   
   static final String wsDirectory = "workspace";
-  private static EclipseHarnessThread eclipseThread = null;
+//  private static EclipseHarnessThread eclipseThread = null;
   
   public EclipseHarness(Config config, File scratch) throws Exception {
     super(config, scratch);
@@ -21,17 +21,18 @@ public class EclipseHarness extends Benchmark {
     super.preIteration(size);
     File wsdir = new File(scratch, wsDirectory);
     wsdir.mkdir();
-    unpackZipFile(fileInScratch("eclipse/plugins/org.eclipse.jdt.core.tests.performance_3.1.2/full-source-R3_0.zip"),wsdir); 
-    
-    if (eclipseThread == null) {
-      eclipseThread = new EclipseHarnessThread(this, "EclipseThread");
-      eclipseThread.start();
-    }
+    unpackZipFile(fileInScratch("eclipse/plugins/org.eclipse.jdt.core.tests.performance_3.1.2/full-source-R3_0.zip"),wsdir);  
   }
   
   public void iterate(String size) throws Exception {
-    EclipseHarnessLock.start(config.getArgs(size));
-  }
+    try {
+      if (!org.eclipse.core.runtime.adaptor.EclipseStarter.isRunning())
+        startup(size);
+      org.eclipse.core.runtime.adaptor.EclipseStarter.run(null);
+    } catch (Exception e) {
+      e.printStackTrace();
+    } 
+  } 
   
   public void postIteration(String size) throws Exception {
     super.postIteration(size);
@@ -40,6 +41,27 @@ public class EclipseHarness extends Benchmark {
   }
   
   public void cleanup() {
-    EclipseHarnessLock.cleanup();
+  }
+
+  private void startup(String size) {
+    try {
+      System.setProperty("osgi.os","linux");
+      System.setProperty("osgi.ws","gtk");
+      System.setProperty("osgi.arch","x86");
+      System.setProperty("osgi.install.area",  "file:"+fileInScratch("eclipse/"));
+      System.setProperty("osgi.noShutdown", "true");
+      System.setProperty("osgi.framework","file:"+fileInScratch("eclipse/plugins/org.eclipse.osgi_3.1.2.jar"));
+      String[] pluginArgs = config.getArgs(size);
+      String[] args = new String[4 + pluginArgs.length];
+      args[0] = "-data";
+      args[1] = fileInScratch("workspace");
+      args[2] = "-application";
+      args[3] = "dacapo.eclipse.dacapoHarness";
+      for (int i = 0; i < pluginArgs.length; i++)
+        args[4+i] = pluginArgs[i];
+      org.eclipse.core.runtime.adaptor.EclipseStarter.startup(args, null);
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
   }
 }
