b4675c779558283edb3fed70f5d6ab95cc7473ee
steveb <steveb@users.sourceforge.net>
2006-08-01 02:27:34 +0000

    More fiddles in anticipation of next release

--- trunk/dacapobench/benchmarks/src/MMTkCallback.java
+++ trunk/dacapobench/benchmarks/src/MMTkCallback.java
@@ -9,12 +9,12 @@ import com.ibm.JikesRVM.memoryManagers.mmInterface.MM_Interface;
 public class MMTkCallback extends Callback {
   /* Immediatly prior to start of the benchmark */
   public void start(String benchmark) {
-    super.start(benchmark);
     MM_Interface.harnessBegin();
+    super.start(benchmark);
   };
   /* Immediatly after the end of the benchmark */
   public void stop() {
-    MM_Interface.harnessEnd();
     super.stop();
+    MM_Interface.harnessEnd();
   }
 }
