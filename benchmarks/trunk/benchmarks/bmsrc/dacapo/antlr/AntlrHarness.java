181756ef747ea10aa74debc53f26aec701815332
rgarner <rgarner@users.sourceforge.net>
2007-12-07 01:00:41 +0000

    Converted chart and batik to the new classloader/build system

--- benchmarks/trunk/benchmarks/bmsrc/dacapo/antlr/AntlrHarness.java
+++ benchmarks/trunk/benchmarks/bmsrc/dacapo/antlr/AntlrHarness.java
@@ -17,15 +17,47 @@ import dacapo.parser.Config;
  */
 public class AntlrHarness extends Benchmark {
 
+  private String[] args;
+  private int firstGrammarIndex;
+  private int iterations;
+  private String[] antlrToolArgs;
+  
   public AntlrHarness(Config config, File scratch) throws Exception {
     super(config,scratch);
     Class<?> clazz = Class.forName("antlr.Tool", true, loader);
     this.method = clazz.getMethod("main", new Class[] { String[].class} );
+
   }
 
   protected void prepare() throws Exception {
     super.prepare();
     copyFileTo(new File(scratch,"antlr/CommonTokenTypes.txt"),scratch);
+  }
+  
+  
+
+  @Override
+  public void prepare(String size) throws Exception {
+    super.prepare(size);
+    String[] args = preprocessArgs(size);
+    firstGrammarIndex = 0;
+    iterations = 1;
+    Vector<String> v = new Vector<String>(args.length);
+    for (int i=0; i < args.length; i++) {
+      if (args[i].equals("-grammars")) {
+        firstGrammarIndex = i+1;
+        antlrToolArgs = new String[v.size()+1];
+        for (int j=0; j < v.size(); j++) {
+          antlrToolArgs[j] = (String)v.elementAt(j);
+        }
+        break;
+      } else if (args[i].equals("-iterations")) {
+        iterations = Integer.parseInt(args[++i]);
+      } else {
+        v.addElement(args[i]);
+      }
+    }
+    this.args = args;
   }
 
   /**
@@ -38,39 +70,16 @@ public class AntlrHarness extends Benchmark {
   }
 
   public void iterate(String size) throws Exception {
-    String[] args = preprocessArgs(size);
-    int firstGrammarIndex = 0;
-    int nIterations = 1;
-    String[] newArgs = null;
-    Vector<String> v = new Vector<String>(args.length);
-    for (int i=0; i < args.length; i++) {
-      if (args[i].equals("-grammars")) {
-        firstGrammarIndex = i+1;
-        newArgs = new String[v.size()+1];
-        for (int j=0; j < v.size(); j++) {
-          newArgs[j] = (String)v.elementAt(j);
-        }
-        break;
-      } else if (args[i].equals("-iterations")) {
-        nIterations = Integer.parseInt(args[++i]);
-      } else {
-        v.addElement(args[i]);
-      }
-    }
-
-    ClassLoader dacapoCL = Thread.currentThread().getContextClassLoader();
-    Thread.currentThread().setContextClassLoader(loader);
-    for (int iteration=0; iteration < nIterations; iteration++) {
+    for (int iteration=0; iteration < iterations; iteration++) {
       for (int iGrammar=firstGrammarIndex;
            iGrammar < args.length;
            iGrammar++) {
         String grammarFile = (new File(scratch,args[iGrammar])).getPath();
-        newArgs[newArgs.length-1] = grammarFile;
+        antlrToolArgs[antlrToolArgs.length-1] = grammarFile;
         System.out.println(args[iGrammar]);
 
-        method.invoke(null, new Object[] {newArgs});
+        method.invoke(null, new Object[] {antlrToolArgs});
       }
     }
-    Thread.currentThread().setContextClassLoader(dacapoCL);
  }
 }
