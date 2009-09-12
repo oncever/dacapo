97af8430a851d0fde6f9fef88e43d5e02b01df2c
steveb-oss <steveb-oss@users.sourceforge.net>
2009-09-12 05:16:20 +0000

    Tagging the 9.10-beta0 release

---
+++ benchmarks/tags/9.10-beta0/benchmarks/bms/fop/src/org/dacapo/harness/Fop.java
@@ -0,0 +1,44 @@
+package org.dacapo.harness;
+
+import java.io.File;
+
+import org.dacapo.harness.Benchmark;
+import org.dacapo.parser.Config;
+
+public class Fop extends Benchmark {
+
+  private String[] args;
+
+  public Fop(Config config, File scratch) throws Exception {
+    super(config, scratch);
+    Class<?> clazz = Class.forName("org.apache.fop.cli.Main", true, loader);
+    this.method = clazz.getMethod("startFOP", new Class[] { String[].class} );
+  }
+
+  @Override
+  protected void prepare(String size) throws Exception {
+    super.prepare(size);
+    args = preprocessArgs(size);
+    /* Retarget input/output files into scratch directory */
+    for (int i=0; i < args.length; i++)
+      if (args[i].charAt(0) != '-')
+        args[i] = fileInScratch(args[i]);
+  }
+
+  public void iterate(String size) throws Exception {
+    method.invoke(null, new Object[] {args});
+  }
+  
+  /**
+   * Stub which exists <b>only</b> to facilitate whole program
+   * static analysis on a per-benchmark basis.  See also the "split-deps"
+   * ant build target, which is also provided to enable whole program
+   * static analysis.
+   * 
+   * @author Eric Bodden
+   */
+  public static void main(String args[]) throws Exception {
+    // create dummy harness and invoke with dummy arguments
+    (new Fop(null, null)).run(null, "");
+  }
+}
---
+++ benchmarks/tags/9.10-beta0/benchmarks/bms/jython/src/org/dacapo/harness/Jython.java
@@ -0,0 +1,59 @@
+package org.dacapo.harness;
+
+import java.io.File;
+import java.lang.reflect.Method;
+
+import org.dacapo.harness.Benchmark;
+import org.dacapo.parser.Config;
+
+
+public class Jython extends Benchmark {
+  
+  private Method pySetArgsMethod;
+  
+  public Jython(Config config, File scratch) throws Exception {
+    super(config, scratch);
+    Class<?> clazz = Class.forName("org.python.util.jython", true, loader);
+    this.method = clazz.getMethod("main", String[].class );
+    Class<?> pyClass = Class.forName("org.python.core.PySystemState", true, loader);
+    pySetArgsMethod = pyClass.getMethod("setArgv", String[].class );
+    System.setProperty("python.home",fileInScratch("jython"));
+    System.setProperty("python.cachedir",fileInScratch("cachedir"));
+    System.setProperty("python.verbose","warning");
+    useBenchmarkClassLoader();
+    try {
+      method.invoke(null, (Object)new String[] {fileInScratch("jython/noop.py")} );
+    } finally {
+      revertClassLoader();
+    }
+  }
+  
+  /**
+   * jython.main doesn't expect to be called multiple times, so we've hacked
+   * Py.setArgv to allow us to reset the command line arguments that the python
+   * script sees.  Hence the Py.setArgv call, followed by the jython.main call.
+   */
+  public void iterate(String size) throws Exception {
+    String[] args = preprocessArgs(size);
+    pySetArgsMethod.invoke(null, (Object) args);
+    method.invoke(null, (Object) args);
+  }
+
+  public void cleanup() {
+    super.cleanup();
+    deleteTree(new File(scratch,"cachedir"));
+  }
+  
+  /**
+   * Stub which exists <b>only</b> to facilitate whole program
+   * static analysis on a per-benchmark basis.  See also the "split-deps"
+   * ant build target, which is also provided to enable whole program
+   * static analysis.
+   * 
+   * @author Eric Bodden
+   */
+  public static void main(String args[]) throws Exception {
+    // create dummy harness and invoke with dummy arguments
+    (new Jython(null, null)).run(null, "");
+  }
+}
